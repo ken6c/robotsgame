9cc2ecae9872c9cc0dafb80c58f2a8ecccb66630
Kenneth <thunderfammedia@gmail.com>
2021-01-04 00:50:18 +0000

    First commit after app was build

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Card({ id,name,email }) {
+    return (
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${ id }?200x200`} alt="robot" />
+            <div>
+                <h3>{ name }</h3>
+                <p>{ email }</p>
+            </div>
+        </div>
+    )
+}
+export default Card;
